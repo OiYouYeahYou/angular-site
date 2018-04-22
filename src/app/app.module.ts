2f53f865cd7a7219569ed0ef7e46a128b6fc7d2e
Jason <github@jallan.co.uk>
2018-04-22 21:44:15 +0100

    [wip] importing content from other site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
 
 import { AppComponent } from './app.component'
+import { AboutComponent } from './about/about.component'
+import { PortfolioComponent } from './portfolio/portfolio.component'
+import { FeedComponent } from './feed/feed.component'
+import { StatusComponent } from './status/status.component'
+import { ContactComponent } from './contact/contact.component'
+import { HomeComponent } from './home/home.component'
 
+
+const appRoutes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'portfolio', component: PortfolioComponent },
+	{ path: 'feed', component: FeedComponent },
+	{ path: 'status', component: StatusComponent },
+	{ path: 'contact', component: ContactComponent },
+]
 
 @NgModule( {
 	declarations: [
-		AppComponent
+		AppComponent,
+		AboutComponent,
+		PortfolioComponent,
+		FeedComponent,
+		StatusComponent,
+		ContactComponent,
+		HomeComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		RouterModule.forRoot( appRoutes )
+	],
+	exports: [
+		RouterModule,
 	],
 	providers: [],
 	bootstrap: [ AppComponent ]
