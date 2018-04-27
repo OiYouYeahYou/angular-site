9249e30f42f42322df2636da2a74dc02a0afa95b
Jason <github@jallan.co.uk>
2018-04-27 14:43:50 +0100

    Removing excess styling

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core'
 
 @Component({
 	selector: 'app-feed',
-	templateUrl: './feed.component.html',
-	styleUrls: ['./feed.component.css']
+	templateUrl: './feed.component.html'
 })
 export class FeedComponent implements OnInit {
 	title = 'home'
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core'
 
 @Component({
 	selector: 'app-portfolio',
-	templateUrl: './portfolio.component.html',
-	styleUrls: ['./portfolio.component.css']
+	templateUrl: './portfolio.component.html'
 })
 export class PortfolioComponent implements OnInit {
 	title = 'home'
