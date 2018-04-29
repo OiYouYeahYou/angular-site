1e2700adaa8d5cf450ac01ebf047e2d66d87f0ff
Jason <github@jallan.co.uk>
2018-04-29 13:23:37 +0100

    Consolodating routing into one file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,15 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component'
-import { AboutComponent } from './about/about.component'
-import { PortfolioComponent } from './portfolio/portfolio.component'
-import { FeedComponent } from './feed/feed.component'
-import { StatusComponent } from './status/status.component'
-import { ContactComponent } from './contact/contact.component'
-import { HomeComponent } from './home/home.component'
-import { NavigationComponent } from './navigation/navigation.component'
-
-const appRoutes: Routes = [
-	{ path: '', component: HomeComponent },
-	{ path: 'home', component: HomeComponent },
-	{ path: 'about', component: AboutComponent },
-	{ path: 'portfolio', component: PortfolioComponent },
-	{ path: 'feed', component: FeedComponent },
-	{ path: 'status', component: StatusComponent },
-	{ path: 'contact', component: ContactComponent }
-]
+import { NavigationComponent, pages } from './navigation/navigation.component'
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		AboutComponent,
-		PortfolioComponent,
-		FeedComponent,
-		StatusComponent,
-		ContactComponent,
-		HomeComponent,
-		NavigationComponent
+		NavigationComponent,
+		...pages.map(page => page.component)
 	],
-	imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+	imports: [BrowserModule, RouterModule.forRoot(pages)],
 	exports: [RouterModule],
 	providers: [],
 	bootstrap: [AppComponent]
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,8 +1,41 @@
 import { Component, OnInit } from '@angular/core'
+import { Routes } from '@angular/router'
+import { HomeComponent } from '../home/home.component'
+import { AboutComponent } from '../about/about.component'
+import { PortfolioComponent } from '../portfolio/portfolio.component'
+import { FeedComponent } from '../feed/feed.component'
+import { StatusComponent } from '../status/status.component'
+import { ContactComponent } from '../contact/contact.component'
+
+interface Route {
+	path: string
+	component: any
+}
+
+export const corePages: Route[] = [
+	{ path: 'home', component: HomeComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'portfolio', component: PortfolioComponent },
+	{ path: 'feed', component: FeedComponent },
+	{ path: 'status', component: StatusComponent },
+	{ path: 'contact', component: ContactComponent }
+]
+
+export const pages: Route[] = [
+	{ path: '', component: HomeComponent },
+	...corePages
+]
+
+const links = corePages.map(
+	({ path }: Route) =>
+		`<a routerLink="/${path}" routerLinkActive="active">${path}</a>`
+)
+
+const template = `<nav>${links.join('\n')}</nav>`
 
 @Component({
 	selector: 'app-navigation',
-	templateUrl: './navigation.component.html'
+	template
 })
 export class NavigationComponent implements OnInit {
 	constructor() {}
